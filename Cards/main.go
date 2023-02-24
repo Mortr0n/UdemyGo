38724ae4b966a2975a895ad898d3f47b844443c8
Mortron <cmsl4y3r@gmail.com>
2023-02-23 18:57:11 -0700

    Playing with conversion to byte slice

--- Cards/main.go
+++ Cards/main.go
@@ -1,16 +1,21 @@
 package main
 
 func main() {
-	cards := newDeck()
+	// cards := newDeck()
 
 	// cards.print()
 	// cards.pizazze()
 
 	// deal returns 2 different deck values
-	hand, remainingCards := deal(cards, 5)
+	// hand, remainingCards := deal(cards, 5)
 
-	hand.print()
-	remainingCards.pizazze()
+	// hand.print()
+	// remainingCards.pizazze()
+
+	// testing type conversion for string to byte slice
+	// greeting := "Hello Mortr0n!"
+	// bSlice := []byte(greeting)
+	// fmt.Println(bSlice)
 }
 
 func newCard() string {
