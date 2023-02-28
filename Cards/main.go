4d0e22fbb26e1f38c324ea81a9b349f3fef20114
Chris Morton <chris.morton@utilisource.us>
2023-02-28 10:29:55 -0700

    Deck Shuffle complete.  On to tests

--- Cards/main.go
+++ Cards/main.go
@@ -1,19 +1,8 @@
 package main
 
 func main() {
-	cards := newDeck()
 
-	// cards.print()
-	// cards.pizazze()
-
-	// deal returns 2 different deck values
-	hand, remainingCards := deal(cards, 5)
-
-	hand.print()
-	remainingCards.pizazze()
-
-}
-
-func newCard() string {
-	return "Five of Diamonds"
+	cards := newDeckFromFile("myCards")
+	cards.shuffle()
+	cards.print()
 }
