caa3fb9cd884895397269261b642231b25406d5a
Chris Morton <chris.morton@utilisource.us>
2023-03-07 10:49:23 -0700

    custom Write implementation

--- interfaces_http/main.go
+++ interfaces_http/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 	resp, err := http.Get("http://www.google.com")
@@ -12,6 +15,22 @@ func main() {
 		fmt.Println("Error: ", err)
 		os.Exit(1)
 	}
+	//!Original or long way to log out the response body for educational purposes
+	// making a byte slice to use for filling with the response body from our request
+	// bs := make([]byte, 99999)
+	// // Body implements the io.Reader interface and needs us to provide a byte slice that it can fill with data
+	// resp.Body.Read(bs)
+	// // remember a byte slice is basically a string so an easy type conversion exists
+	// fmt.Println(string(bs))
+	//! End of Long way.
+	lw := logWriter{}
 
-	fmt.Println(resp.Body)
+	io.Copy(lw, resp.Body)
+}
+
+// Making our own custom implementation of the write interface with the logWriter struct from above
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes: ", len(bs))
+	return len(bs), nil
 }
